255501931bd2de02f009a1f248ea31fa141d0b54
Danitocode <felipedanielc248@gmail.com>
2021-02-08 15:37:12 +0100

    Land page body and slider ended.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from "react";
 import Header from "../src/components/Header";
 import DroneFetch from "../src/components/DroneFetch";
 import Footer from "../src/components/Footer";
+import Slider from "../src/components/Slider";
 
 
 
 function App() {
   return (<>
       <div className="container">
-      <Header />
+      <header>
+        <Header />
+        <Slider />
+      </header>
+
       <DroneFetch className="tab-pane fade show active" id="nav-films" role="tabpanel" aria-labelledby="nav-home-tab" />
       
       <Footer />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 const nav = {
   border: "solid",
@@ -8,39 +7,9 @@ const nav = {
 };
 
 class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selected: 0,
-      currentImage: this.props.images[0],
-    };
-  }
-
-  componentDidMount() {
-    this.intervalID = setInterval(() => this.onChangePosition(null, 1), 3000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.intervalID);
-  }
-
-  onChangePosition(e, value) {
-    const maxValue = this.props.images.length;
-    let current = this.state.selected;
-    current = (current + value) % maxValue;
-    if (current < 0) {
-      current = maxValue - 1;
-    }
-    this.setState({
-      selected: current,
-      currentImage: this.props.images[current],
-    });
-  }
-
   render() {
-    const { currentImage } = this.state;
     return (
-      <header>
+
         <nav
           class="navbar navbar-expand-lg navbar-light bg-light py-md-2"
           style={{ nav }}>
@@ -139,28 +108,12 @@ class Header extends React.Component {
           </div>
         </nav>
 
-        <div
-          class="jumbotron jumbotron-image color-grey-light"
-          style={{ backgroundImage: `url('${currentImage}')` }}
-        >
-          <div class="mask rgba-black-strong d-flex align-items-center h-100">
-            <div class="container text-center white-text py-5">
-              <h1 class="mb-6">The best drone shop</h1>
-            </div>
-          </div>
-        </div>
-      </header>
+
+
     );
   }
 }
 
 export default Header;
-Header.propTypes = {
-  images: PropTypes.array,
-};
-Header.defaultProps = {
-  images: [
-    "/images/drone1.jpg",
-    "https://i.picsum.photos/id/859/1900/250.jpg?hmac=-qp63LmIlFnEi-PAzNbLJ6rxB_glW9hP1zzBrHnVvL0",
-  ],
-};
+
+
